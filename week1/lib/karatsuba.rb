a68e25b4d2a0500ea3c9c593ec0b66a573117769
Tim Millar <heteroskedasticity99@gmail.com>
2016-11-30 00:04:25 +0000

    Refactored Karatsuba multiply method

--- week1/lib/karatsuba.rb
+++ week1/lib/karatsuba.rb
@@ -15,38 +15,29 @@ class Karatsuba
     if small_enough?
       x * y
     else
-      n = size_of_smallest_number
+      z_2 = left_x * left_y
+      z_0 = Karatsuba.multiply(right_x, right_y)
+      z_1 = Karatsuba.multiply(left_x+right_x, left_y+right_y) - z_2 - z_0
 
-      i = x.to_s.size - n
-      a = x.to_s[0..(i-1)].to_i
-      b = x.to_s[i..-1].to_i
-      j = y.to_s.size - n
-      c = y.to_s[0..(j-1)].to_i
-      d = y.to_s[j..-1].to_i
-
-      z_2 = Karatsuba.multiply(a,c)
-      z_0 = Karatsuba.multiply(b,d)
-      z_1 = Karatsuba.multiply(a+b, c+d) - z_2 - z_0
-
-      (10**(2*n))*z_2 + (10**n)*z_1 + z_0
+      left_padding*z_2 + mid_padding*z_1 + z_0
     end
   end
 
   private
 
-  def size_of_smallest_number
-    [x.to_s.size, y.to_s.size].min - 1
+  def digits_in_smallest_number
+    [size_of_x, size_of_y].min - 1
   end
 
   def small_enough?
-    x_small_enough? || y_emall_enough?
+    x_small_enough? || y_small_enough?
   end
 
   def x_small_enough?
     size_of_x < 2
   end
 
-  def y_emall_enough?
+  def y_small_enough?
     size_of_y < 2
   end
 
@@ -56,5 +47,45 @@ class Karatsuba
 
   def size_of_y
     y.to_s.size
+  end
+
+  def x_cutoff
+    size_of_x - digits_in_smallest_number
+  end
+
+  def y_cutoff
+    size_of_y - digits_in_smallest_number
+  end
+
+  def left_padding
+    10**(2*digits_in_smallest_number)
+  end
+
+  def mid_padding
+    10**digits_in_smallest_number
+  end
+
+  def x_string
+    x.to_s
+  end
+
+  def y_string
+    y.to_s
+  end
+
+  def left_x
+    x_string[0..(x_cutoff-1)].to_i
+  end
+
+  def right_x
+    x_string[x_cutoff..-1].to_i
+  end
+
+  def left_y
+    y_string[0..(y_cutoff-1)].to_i
+  end
+
+  def right_y
+    y_string[y_cutoff..-1].to_i
   end
 end
