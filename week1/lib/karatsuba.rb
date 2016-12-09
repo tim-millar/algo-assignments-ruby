b194fe603178f37087f0e88f2c2c8c1f84b9e6ad
Tim Millar <heteroskedasticity99@gmail.com>
2016-12-09 21:01:30 +0000

    Refactored karatsuba class.

--- week1/lib/karatsuba.rb
+++ week1/lib/karatsuba.rb
@@ -1,23 +1,49 @@
 class Karatsuba
+  attr_reader :x, :y
+  private :x, :y
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
 
   def self.multiply(x, y)
+    new(x, y).multiply
+  end
 
-    if x.to_s.size || y.to_x.size
+  def multiply
+    if x_is_one_digit? || y_is_one_digit?
       x * y
     else
-      n = [x.to_s.size, y.to_s.size].max
-      m = n / 2
+      a = x / 10**half_min_digit_size
+      b = x % 10**half_min_digit_size
+      c = y / 10**half_min_digit_size
+      d = y % 10**half_min_digit_size
 
-      a = x / 10**m
-      b = x % 10**m
-      c = y / 10**m
-      d = y % 10**m
+      ac = Karatsuba.multiply(a,c)
+      bd = Karatsuba.multiply(b,d)
+      ad_plus_bc = Karatsuba.multiply(a+b,c+d) - ac - bd
 
-      ac = multiply(a,c)
-      bd = multiply(b,d)
-      ad_plus_bc = multiply(a+b,c+d) - ac - bd
-
-      ac * 10**(2*m) + (ad_plus_bc * 10**m) + bd
+      ac * 10**(2*half_min_digit_size) + (ad_plus_bc * 10**half_min_digit_size) + bd
     end
   end
+
+  private
+
+  def minimum_digit_size
+    [x.to_s.size, y.to_s.size].max
+  end
+
+  def half_min_digit_size
+    minimum_digit_size / 2
+  end
+
+  def x_is_one_digit?
+    x.to_s.size == 1
+  end
+
+  def y_is_one_digit?
+    y.to_s.size == 1
+  end
+
 end
